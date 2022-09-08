f9f74e5b324ccec7c41365afaf95a20164238874
Anton <88505337+finchren@users.noreply.github.com>
2022-09-08 16:22:31 +0300

    Reworked the code

--- 205. Isomorphic Strings.c
+++ 205. Isomorphic Strings.c
@@ -1,24 +1,23 @@
+// Task - https://leetcode.com/problems/isomorphic-strings/
+
 // Copyrigth 2022 School 21 finchren
 
 bool isIsomorphic(char * s, char * t){
     // Used as final return value
-    bool are_isomorphic = false;
-    // I compare the previous and the next value of chars using these variables
-    int chars_are_equal_s = 0, chars_are_equal_t = 0;
-    char previous_s = '\0', previous_t = '\0';
+    bool are_isomorphic = 0;
     int index_upper = 0;
     // While chars exist, I iterate over the strings
     // Since the length is equal by the constraints of the task, I need to check only one
     while (s[index_upper]) {
         
-        previous_s = s[index_upper];
-        previous_t = t[index_upper];
-        
         int index_inner = 0;
         while (s[index_inner]) {
+            // I compare the previous and the next value of chars using these variables
+            int chars_are_equal_s = 0, chars_are_equal_t = 0;
+
             // If previous and next are equal, then 1, else 0
-            chars_are_equal_s = previous_s == s[index_inner] ? 1 : 0;
-            chars_are_equal_t = previous_t == t[index_inner] ? 1 : 0;
+            chars_are_equal_s = s[index_upper] == s[index_inner] ? 1 : 0;
+            chars_are_equal_t = t[index_upper] == t[index_inner] ? 1 : 0;
             // If the strings don't have the same squence of equal/unequal chars, I stop and return
             if (chars_are_equal_s != chars_are_equal_t) {
                 return are_isomorphic;
