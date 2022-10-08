4b153c129aa76146f9254aab4b8cb909c1184536
Anton <88505337+finchren@users.noreply.github.com>
2022-10-08 13:13:47 +0300

    Reworked the solution

--- 121. Best Time to Buy and Sell Stock.c
+++ 121. Best Time to Buy and Sell Stock.c
@@ -4,13 +4,22 @@ int maxProfit(int* prices, int pricesSize){
     
     int max_profit = 0, min = 0, max = 0;
     
+    min = prices[0];
+    
     // Iterate over the elements of the prices one by one
-    for (int i = 0; i < pricesSize; ++i) {
+    for (int i = 1; i < pricesSize; ++i) {
         
-        min = prices[i];
-        // Compare the difference with the current max profit value and overwrite it if it is greater than the previous one
-        for (int j = i + 1; j < pricesSize; ++j) {
-            max_profit = (prices[j] - prices[i]) > max_profit ? (prices[j] - prices[i]) : max_profit;
+        // If the current element of prices is less than min I compare the max_profit
+        // And if it is greater than the previous than the previous value, overwrite the max_profit
+        if (prices[i] > min) {
+            
+            max = prices[i];
+            max_profit = max - min >= max_profit ? max - min : max_profit;
+            
+        } else {
+            
+            min = prices[i];
+            
         }
         
     }
