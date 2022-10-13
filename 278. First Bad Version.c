95a04a5795519792375ef77b592c0cf0a2de2e08
Anton <88505337+finchren@users.noreply.github.com>
2022-10-13 19:29:42 +0300

    Reworked using middle not current and previous

--- 278. First Bad Version.c
+++ 278. First Bad Version.c
@@ -2,38 +2,19 @@
 // bool isBadVersion(int version);
 
 int firstBadVersion(int n) {
+   
+    unsigned int current = 1, middle = 0;
     
-    unsigned int current = 1;
-    
-    // Until I find the bad version I multiply the current by 2
-    while (!isBadVersion(current) && current <= n) {
-        current *= 2;
-    }
-    
-    // If the value is greater than max int, set current to max int
-    if (current > INT_MAX) {
-        current = INT_MAX;
-    }
-    
-    unsigned int previous = current - 1;
-    
-    while (isBadVersion(previous / 2) && previous >= 1) {
-        previous /= 2;
-    }
-    
-    if (isBadVersion(previous) && isBadVersion(current)) {
-        while (isBadVersion(previous) && isBadVersion(current)) {
-            --previous;
-            --current;
-        } 
-    }
-    
-    if (!isBadVersion(previous) && isBadVersion(current)) {
-        while (!isBadVersion(previous) && isBadVersion(current)) {
-            ++previous;
-            --current;
+    while (current < n) {
+        
+        middle = (current + n) / 2;
+        
+        if (isBadVersion(middle)) {
+            n = middle;
+        } else {
+            current = middle + 1;
         }
     }
-        
-    return ++current;
+    
+    return current;
 }
