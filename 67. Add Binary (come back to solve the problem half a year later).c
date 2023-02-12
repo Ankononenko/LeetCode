2f4848b5a65c2af5cead076a7e9d178f55c9d243
Anton <88505337+finchren@users.noreply.github.com>
2023-02-12 11:31:26 +0300

    Finally solved, Runtime 0 ms Beats 100%, Memory 5.7 MB Beats 83.57%

--- 67. Add Binary (come back to solve the problem half a year later).c
+++ 67. Add Binary (come back to solve the problem half a year later).c
@@ -1,5 +1,5 @@
 int find_len(char* string);
-int are_all_ones(char* string, int start_index, int end_index);
+int are_all_ones(char* a, char* b, int a_len, int b_len);
 
 char * addBinary(char * a, char * b){
     
@@ -11,16 +11,12 @@ char * addBinary(char * a, char * b){
     // 1 for \0 at the end of the line;
     int extra_place = 1, diff = 0, all_ones = 0;
     if (a[0] == '1' && b[0] == '1') {
-        if (a_len > b_len) {
+        if (a_len > b_len || b_len > a_len) {
+            all_ones = are_all_ones(a, b, a_len, b_len);
             // +2 for extra char - remainder;
-            all_ones = are_all_ones(a, b_len, a_len);
-            // HERE
-            extra_place = a[b_len] == '1' && all_ones ? 2 : 1;
-        } else if (b_len > a_len) {
-            all_ones = are_all_ones(b, a_len, b_len);
-            // HERE
-            extra_place = b[a_len] == '1' && all_ones ? 2 : 1;
+            extra_place = all_ones ? 2 : 1;
         } else if (a_len == b_len) {
+            all_ones = 1;
             extra_place = 2;
         }
     }
@@ -36,13 +32,16 @@ char * addBinary(char * a, char * b){
     char* ret_arr;
     ret_arr = (char*) malloc (new_arr_len * sizeof(char));
 
-    printf("extra = %d, new_arr_len = %d\n", extra_place, new_arr_len);
-
-
     char remainder = '0', insert_char = '0';
-    // HERE
-    int insert_index = new_arr_len - extra_place;
-    printf("insert_index = %d\n", insert_index);
+
+    int insert_index = 0;
+    if ((a_len > b_len || b_len > a_len) && !all_ones) {
+        insert_index = new_arr_len - 1 - extra_place;
+    } else if (a_len == 1 && b_len == 1 && !all_ones) {
+        insert_index = 0;
+    } else {
+        insert_index = new_arr_len - extra_place;
+    }
 
     while (index_a >= 0 && index_b >= 0) {
         if (a[index_a] == '0' && b[index_b] == '0' && remainder == '0') {
@@ -68,7 +67,6 @@ char * addBinary(char * a, char * b){
             remainder = '1';
         }
         ret_arr[insert_index--] = insert_char;
-        printf("a & b insert_char = %d\n", insert_char);
         --index_a;
         --index_b;
     }
@@ -87,7 +85,6 @@ char * addBinary(char * a, char * b){
         }
         ret_arr[insert_index--] = insert_char;
         --index_a;
-        printf("a insert_char = %d\n", insert_char);
     }
 
     while (index_b >= 0) {
@@ -103,7 +100,6 @@ char * addBinary(char * a, char * b){
             insert_char = '0';
         }
         ret_arr[insert_index--] = insert_char;
-        printf("b insert_char = %d\n", insert_char);
         --index_b;
     }
 
@@ -116,24 +112,42 @@ char * addBinary(char * a, char * b){
     return ret_arr;
 }
 
-int are_all_ones(char* string, int start_index, int end_index) {
-    int all_ones = 1;
-    for (int index = start_index; index < end_index; ++index) {
-        if (string[index] != '1') {
-            all_ones = 0;
-            break;
+int are_all_ones(char* a, char* b, int a_len, int b_len) {
+    int all_ones = 1, index_a = a_len - 1, index_b = b_len - 1;
+    char remainder = '0';
+    while (index_a >= 0 && index_b >= 0) {
+        if (a[index_a] == '0' && b[index_b] == '0' && remainder == '1') {
+            remainder = '0';
+        } else if (a[index_a] == '1' && b[index_b] == '1' && remainder == '0') {
+            remainder = '1';
         }
+        --index_a;
+        --index_b;
+    }
+    while (index_a >= 0) {
+        if (a[index_a] == '0' && remainder == '1') {
+            remainder = '0';
+        }
+        --index_a;
+    }
+    while (index_b >= 0) {
+        if (b[index_b] == '0' && remainder == '1') {
+            remainder = '0';
+        }
+        --index_b;
+    }
+    if (remainder == '1') {
+        all_ones = 1;
+    } else {
+        all_ones = 0;
     }
     return all_ones;
 }
 
 int find_len(char* string) {
-    
     int len = 0;
-    
     while (string[len]) {
         ++len;
     }
-    
     return len;
 }
