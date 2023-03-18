ff2a32af689548bcb716ad0f96ada41c98e73b2b
Anton <88505337+finchren@users.noreply.github.com>
2023-03-18 11:40:18 +0300

    Daily exercise

---
+++ 1929. Concatenation of Array.c
@@ -0,0 +1,12 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getConcatenation(int* nums, int numsSize, int* returnSize){
+    *returnSize = numsSize * 2;
+    int* res = (int*)malloc(*returnSize * sizeof(int));
+    for (int i = 0; i < numsSize; ++i) {
+        res[i] = nums[i];
+        res[i + numsSize] = nums[i];
+    }
+    return res;
+}
